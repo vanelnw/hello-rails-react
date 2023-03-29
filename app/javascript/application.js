fd21f2e90c5ca200672d330b94adb1186106e3f3
Vanel <vanelnwaba@gmail.com>
2023-03-29 02:04:58 +0100

    add react

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,7 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
