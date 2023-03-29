ef0439b55dc8e28c3d8aa40c9a2197bc11ee0894
Vanel <vanelnwaba@gmail.com>
2023-03-29 02:07:44 +0100

    add redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,6 +11,6 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
 );
